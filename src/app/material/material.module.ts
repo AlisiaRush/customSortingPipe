10e239ea6dbc4a26f7db1816ef91865247fa562d
alisia rush <alisiarush@gmail.com>
2019-08-19 22:48:08 -0400

    Added Search MultiSelect Dropdown Menu

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 
 @NgModule({
   declarations: [],
@@ -10,7 +12,9 @@ import { MatSelectModule } from '@angular/material/select';
   ],
   exports:[
     MatToolbarModule,
-    MatSelectModule
+    MatSelectModule,
+    MatFormFieldModule,
+    NgxMatSelectSearchModule
   ]
 })
 export class MaterialModule { }
--- src/app/sort-select-menu/sort-select-menu.component.ts
+++ src/app/sort-select-menu/sort-select-menu.component.ts
@@ -1,7 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Pipe } from '@angular/core';
-import * as moment from 'moment';
+import { ReplaySubject, Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+import { MatSelect } from '@angular/material';
+
+export interface fillSearch {
+  msName: string;
+
+}
+
+/** list of fillSearches */
+export const SELECTED_SEARCHES: fillSearch[] = [
+  {msName: 'Tom Cruise'},
+  {msName: 'Tom Hank'},
+  {msName: 'Henry Robinson'},
+  {msName: 'Idris Elba'},
+  {msName: 'Alisia Rush'},
+  {msName: 'Jack WhoKnows'}
+];
+
+/** list of fillSearch groups */
 
 @Pipe({
   name: 'orderBy'
@@ -12,16 +32,84 @@ import * as moment from 'moment';
   templateUrl: './sort-select-menu.component.html',
   styleUrls: ['./sort-select-menu.component.css']
 })
-export class SortSelectMenuComponent implements OnInit {
-  search: any =[]
+export class SortSelectMenuComponent implements OnInit, AfterViewInit, OnDestroy {
+  search: any = []
+  searchMulti = []
   order = "name";
   ascending = false;
   constructor(private http: HttpClient) {}
 
+  protected fillSearches: fillSearch[] = SELECTED_SEARCHES;
+
+  /** control for the selected fillSearches for multi-selection */
+  public searchSelectMultiCtrl: FormControl = new FormControl();
+
+  /** control for the MatSelect filter keyword multi-selection */
+  public searchSelectMultiFilterCtrl: FormControl = new FormControl();
+
+  /** list of fillSearches filtered by search keyword */
+  public filteredSearchMulti: ReplaySubject<fillSearch[]> = new ReplaySubject<fillSearch[]>(1);
+
+  @ViewChild('multiSelect',{static: false}) multiSelect: MatSelect;
+
+  /** Subject that emits when the component has been destroyed. */
+  protected _onDestroy = new Subject<void>();
+
+
   ngOnInit() {
+   // set initial selection
+   this.searchSelectMultiCtrl.setValue([this.fillSearches[10], this.fillSearches[11], this.fillSearches[12]]);
+
+   // load the initial fillSearch list
+   this.filteredSearchMulti.next(this.fillSearches.slice());
+
+   // listen for search field value changes
+   this.searchSelectMultiFilterCtrl.valueChanges
+     .pipe(takeUntil(this._onDestroy))
+     .subscribe(() => {
+       this.filterBanksMulti();
+     });
   }
 
-search2(prop: string){
+  ngAfterViewInit() {
+    this.setInitialValue();
+  }
+
+  ngOnDestroy() {
+    this._onDestroy.next();
+    this._onDestroy.complete();
+  }
+
+  /**
+   * Sets the initial value after the filteredBanks are loaded initially
+   */
+  protected setInitialValue() {
+    this.filteredSearchMulti
+      .pipe(take(1), takeUntil(this._onDestroy))
+      .subscribe(() => {
+        this.multiSelect.compareWith = (a: fillSearch, b: fillSearch) => a && b && a.msName === b.msName;
+      });
+  }
+
+  protected filterBanksMulti() {
+    if (!this.fillSearches) {
+      return;
+    }
+    // get the search keyword
+    let search = this.searchSelectMultiFilterCtrl.value;
+    if (!search) {
+      this.filteredSearchMulti.next(this.fillSearches.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the fillSearches
+    this.filteredSearchMulti.next(
+      this.fillSearches.filter(msSearch => msSearch.msName.toLowerCase().indexOf(search) > -1)
+    );
+  }
+
+search2(){
   this.http.get('https://jsonplaceholder.typicode.com/users/')
   .subscribe((data)=>{
     this.search = data;
